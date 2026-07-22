d59924df19e5bff11aac1c9c2477f7975e97a97f
Imran Shaik <99525273+shaikimran264@users.noreply.github.com>
2026-07-22 11:27:43 +0530

    Step 13: other_player

--- model.py
+++ model.py
@@ -317,8 +317,13 @@ def is_terminal(board):
         
     return (False,0)
 
-# Step 13 - other_player (not yet solved)
-# TODO: implement
+# Step 13 - other_player
+def other_player(player):
+    # TODO: return the opponent's player code (1 <-> 2)
+    # pass
+    if player==1:
+        return 2
+    return 1
 
 # Step 14 - step_env (not yet solved)
 # TODO: implement
